fa462ec03c4fd65f29fb9ef03a1ce4aa3416fd37
Paul <46579053+pauburk@users.noreply.github.com>
2021-06-14 15:43:38 -0700

    completed hw0pr1

--- hw0pr1.py
+++ hw0pr1.py
@@ -1,8 +1,29 @@
 # CS5 Gold/Black: Lab 0, Problem 1
 # Filename: hw0pr1.py
-# Name:
+# Name: Paul Burke
 # Problem description: The four fours challenge!
 
 from math import *
 
 print("Zero is", 4+4-4-4)
+print("One is", 4/4*4/4)
+print("Two is", factorial(4)/(4+4+4))
+print("Three is", (4*4-4)/4)
+print("Four is", sqrt(4)*sqrt(4)*(4/4))
+print("Five is", (4*4+4)/4)
+print("Six is", ((4+4)/4)+4)
+print("Seven is", 4+4-(4/4))
+print("Eight is", 4*4/4+4)
+print("Nine is", 4+4+(4/4))
+print("Ten is", 44/(4.4))
+print("Eleven is", factorial(4)/sqrt(4)-(4/4))
+print("Twelve is", factorial(4)-4-4-4)
+print("Thirteen is", factorial(4)/sqrt(4)+(4/4))
+print("Fourteen is", 4+4+4+sqrt(4))
+print("Fifteen is", 4*4-(4/4))
+print("Sixteen is", 4**4/4/4)
+print("Seventeen is", 4*4+(4/4))
+print("Eighteen is", factorial(4)-4-4+sqrt(4))
+print("Nineteen is", factorial(4)-4-(4/4))
+print("Twenty is", factorial(4)-4*(4/4))
+print("Twenty-one is", factorial(4)-4+(4/4))
